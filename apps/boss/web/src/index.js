465d74975f88fc2710503443793fe5f86f9308ff
Rajesh05SahayogiONe <rajesh@sahayogione.com>
2026-07-06 15:37:04 +0530

    edit index.js file

--- apps/boss/web/src/index.js
+++ apps/boss/web/src/index.js
@@ -1,3 +1,5 @@
 // Placeholder surface for the "boss" product (mirrors apps/boss/web in sahayogi_one).
 export const product = 'boss';
 export const surface = 'web';
+
+console.log("this is the boss src file")
