14a62d24174eb5bcabbed2daa74635a57cbc2fce
Kanishka Garg <kanishkagarg@Kanishkas-MacBook-Air.local>
2026-07-10 18:17:32 +0530

    kanishak

--- apps/tax/web/src/index.js
+++ apps/tax/web/src/index.js
@@ -2,4 +2,4 @@
 export const product = 'tax';
 export const surface = 'web';
 
-console.log("kanishka change the code repo at tax folder ")
+console.log("kanishka change the code repo at tax folder repo ")
